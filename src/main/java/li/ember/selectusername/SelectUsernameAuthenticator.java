efd8193253b746989bf1e02558999ebf7c857a39
transcaffeine <transcaffeine@finally.coffee>
2023-12-27 20:15:40 +0100

    chore: update to keycloak 22 and above
    This migrates to Quarkus 3 (KC22), which switched
    from Java EE to Jakarta

--- src/main/java/li/ember/selectusername/SelectUsernameAuthenticator.java
+++ src/main/java/li/ember/selectusername/SelectUsernameAuthenticator.java
@@ -5,8 +5,8 @@ import org.keycloak.authentication.*;
 import org.keycloak.models.*;
 import org.keycloak.provider.ProviderConfigProperty;
 
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
